9dd2b889d20fab06bc1a519c7a78b9bd0ccf8ccb
Meena Yadav <148996539+sansatjanish01@users.noreply.github.com>
2023-10-28 18:42:32 +0000

    finished my practical 1 and 2

--- practical_01.py
+++ practical_01.py
@@ -1,1 +1,9 @@
-print("Hey , How are you !")
+def calculation (a,b):
+    if a != 0 and b !=0:
+        print(f"The addition of {a} and {b} is",a+b)
+        print(f"The Substraction of {a}and {b} is ",a-b)
+        print(f"The Multiplication of {a} and {b} is ",a*b)
+        print(f"The Divsion of {a} and {b} is ",a/b)
+user_input_a = int(input("Please enter Yout First number :"))
+user_input_b = int(input("Please enter Your Second number :"))
+calculation(user_input_a,user_input_b)
