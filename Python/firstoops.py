76c20a66bf242592f61fe944855da32520874782
Meena Yadav <148996539+sansatjanish01@users.noreply.github.com>
2023-10-30 17:59:05 +0000

    first Program using oop's

---
+++ Python/firstoops.py
@@ -0,0 +1,13 @@
+class college:
+    def __init__(self,name,roll_no,contact,addresh):
+        self.name = name
+        self.roll_no = roll_no
+        self.contact = contact
+        self.addresh = addresh
+    def info(self):
+        print(f"My name is {self.name} and my roll no is {self.roll_no} and this is my contact number {self.contact} and i used to live near {self.addresh}")
+a = college("sansat",101,999999999," new link raod ic colony borivali west")
+a.info()
+print()
+b = college("alam",102,9876543210,"shubam nagar road malad west")
+b.info()
