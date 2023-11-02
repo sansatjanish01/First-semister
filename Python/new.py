1dc99e2531a9833c82001620254a84724e4d4daf
Meena Yadav <148996539+sansatjanish01@users.noreply.github.com>
2023-11-02 17:21:26 +0000

    mini calculatioon using numpy

--- Python/new.py
+++ Python/new.py
@@ -1,13 +1,14 @@
-import pandas as pd
 import numpy as np
-
-dat = {
-    'car':["BMW","Volvo","Shift","Nishan"],
-    'age':[3,5,4,3],
-    'model':[2021,2018,2023,2020],
-    'color':["red","green","black","white"]
-}
-vari = pd.DataFrame(dat)
-print(vari)
-print(type(vari))
-print(type(dat))
+mat_1 = [[3,3,5,7],[4,6,8,3]]
+mat_2 = [[54,43,65,76],[44,65,34,34]]
+operation = input("(add/sub/mul/div) choose one of the operation : ")
+if operation == "add":
+    print(np.add(mat_2, mat_1))
+elif operation == "sub":
+    print(np.subtract(mat_1,mat_2))
+elif operation == "mul":
+    print(np.multiply(mat_1,mat_2))
+elif operation == "div":
+    print(np.divide(mat_1,mat_2))
+else:
+    print("invalid input")
